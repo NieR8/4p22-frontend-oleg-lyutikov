9a01ee5891059f42290bfdb34bac54fc3070b581
Oleg Lutikov <olimpiadic@mail.ru>
2022-11-02 00:22:46 +0700

    add homework_js_2

---
+++ homework_js_2/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+function fib() {
+	let f1 = 0
+	let f2 = 1
+	console.log(f1)
+	return function() {
+		let res
+		res = f1 + f2
+		f1 = f2
+		f2 = res
+		console.log(res)
+	}
+}
+
+const fibonacci = fib()
+
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
+fibonacci()
